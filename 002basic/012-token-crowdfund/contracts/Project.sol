d4613488d45a71bd5bb850ed320967f603286105
jiasenwqbr <jasonjiasen@gmail.com>
2023-11-27 11:20:50 +0800

    Function to fund a certain project.Function to change the project state depending on conditions.Function to give the received funds to project starter.Function to retrieve donated amount when a project expires.Function to get specific information about the project.

--- 002basic/012-token-crowdfund/contracts/Project.sol
+++ 002basic/012-token-crowdfund/contracts/Project.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: MIT 
 pragma solidity 0.8.23;
 import "./SafeMath.sol";
 contract Project {
+    using SafeMath for uint256;
     // Data structures
     enum State {
         Fundraising,
@@ -8,7 +10,7 @@ contract Project {
         Successful
     }
     // State variables
-    address payable public creator;
+    address payable  public creator;
     uint public amountGoal;
     uint public complateAt;
     uint256 public currentBalance;
@@ -33,8 +35,7 @@ contract Project {
         require(msg.sender == creator);
         _;
     }
-    constructor
-    (
+    constructor (
         address payable projectStarter,
         string memory projectTitle,
         string memory projectDesc,
@@ -47,6 +48,78 @@ contract Project {
         amountGoal = goalAmount;
         raiseBy = fundRaisingDeadline;
         currentBalance = 0;
+    }
+    /** @dev Function to fund a certain project.为某个项目提供资金的功能。
+      */
+    function contribute() external inState(State.Fundraising) payable {
+        require(msg.sender != creator);
+        contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+        currentBalance = currentBalance.add(msg.value);
+        emit FundingReceived(msg.sender, msg.value, currentBalance);
+        checkIfFundingCompleteOrExpired();
+    }
+    /** @dev Function to change the project state depending on conditions.
+     * 根据条件更改项目状态的函数。
+      */
+    function checkIfFundingCompleteOrExpired() public {
+        if (currentBalance >= amountGoal) {
+            state = State.Successful;
+        } else if (block.timestamp > raiseBy){
+            state = State.Expired;
+        } 
+        complateAt = block.timestamp;
+    }
+     /** @dev Function to give the received funds to project starter.
+      * 将收到的资金交给项目启动人的功能。
+      */
+    function payOut() internal inState(State.Successful) returns (bool){
+        uint256 totalRaised = currentBalance;
+        currentBalance = 0;
+        if (creator.send(totalRaised)){
+            emit CreatorPaid(creator);
+            return true;
+        } else {
+            currentBalance = totalRaised;
+            state = State.Successful;
+        }
+        return false;
+    }
+    /** @dev Function to retrieve donated amount when a project expires.
+     * 用于在项目过期时获取捐赠金额。
+      */
+
+    function getRefund() public inState(State.Expired) returns (bool) {
+        require(contributions[msg.sender] > 0);
+        uint amountToRefund = contributions[msg.sender];
+        address payable recipient = payable(msg.sender);
+        if (recipient.send(amountToRefund)){
+            contributions[msg.sender] = amountToRefund;
+            return false;
+        } else {
+            currentBalance = currentBalance.sub(amountToRefund);
+        }
+        return true;
+    }
+    /** @dev Function to get specific information about the project.
+      * Returns all the project's details
+      */
+    function getDetails() public view returns
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint256 deadline,
+        State currentState,
+        uint256 currentAmount,
+        uint256 goalAmount
+    ) {
+        projectStarter = creator;
+        projectTitle = title;
+        projectDesc = description;
+        deadline = raiseBy;
+        currentState = state;
+        currentAmount = currentBalance;
+        goalAmount = amountGoal;
     }
 
 
