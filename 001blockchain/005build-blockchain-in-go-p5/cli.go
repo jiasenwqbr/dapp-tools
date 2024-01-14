0b5d880ac0406eb8b7ba309047e395ba9df6485f
jiasenwqbr <jasonjiasen@gmail.com>
2024-01-14 22:51:35 +0800

    Building Blockchain in Go Part 5: Addresses

--- 001blockchain/005build-blockchain-in-go-p5/cli.go
+++ 001blockchain/005build-blockchain-in-go-p5/cli.go
@@ -5,31 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 // CLI responsible for processing command line arguments
 type CLI struct{}
-
-func (cli *CLI) createBlockchain(address string) {
-	bc := CreateBlockchain(address)
-	bc.db.Close()
-	fmt.Println("Done!")
-}
-
-func (cli *CLI) getBalance(address string) {
-	bc := NewBlockchain(address)
-	defer bc.db.Close()
-
-	balance := 0
-	UTXOs := bc.FindUTXO(address)
-
-	for _, out := range UTXOs {
-		balance += out.Value
-	}
-
-	fmt.Printf("Balance of '%s': %d\n", address, balance)
-}
 
 func (cli *CLI) printUsage() {
 	fmt.Println("Usage:")
@@ -44,37 +23,6 @@ func (cli *CLI) validateArgs() {
 		cli.printUsage()
 		os.Exit(1)
 	}
-}
-
-func (cli *CLI) printChain() {
-	// TODO: Fix this
-	bc := NewBlockchain("")
-	defer bc.db.Close()
-
-	bci := bc.Iterator()
-
-	for {
-		block := bci.Next()
-
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
-}
-
-func (cli *CLI) send(from, to string, amount int) {
-	bc := NewBlockchain(from)
-	defer bc.db.Close()
-
-	tx := NewUTXOTransaction(from, to, amount, bc)
-	bc.MineBlock([]*Transaction{tx})
-	fmt.Println("Success!")
 }
 
 // Run parses command line arguments and processes commands
