dc230569b582decd272b40dedf28511d1e26ce34
Jason <jasonjiasen@gmail.com>
2025-04-05 08:54:58 +0800

    init

---
+++ 003solana/004Solana trainning/code/bootcamp-temp/bootcamp-temp/anchor/programs/voting/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod voting {
+    use super::*;
+}
---
+++ 003solana/004Solana trainning/code/bootcamp-temp/bootcamp-temp/anchor/target/types/voting.ts
@@ -0,0 +1,16 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/voting.json`.
+ */
+export type Voting = {
+  "address": "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF",
+  "metadata": {
+    "name": "voting",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": []
+};
